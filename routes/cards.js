d78b1f17143c2ba0e5716e3157008a22bbc9ea23
raaarwwwwandy <hirandysan@gmail.com>
2017-03-22 18:24:38 -1000

    routes are working now

---
+++ routes/cards.js
@@ -0,0 +1,52 @@
+/*jshint esversion:6 */
+
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+let Cards = db.Card;
+
+router.route('/')
+  .get ( (req, res) =>{ 
+    Cards.findAll()
+      .then( cards =>{
+        res.json({cardsList:{cards}});
+      });
+  })
+
+  .post( (req, res) =>{
+    Cards.create({
+      title : req.body.title,
+      priority: req.body.priority,
+      created_by: req.body.created_by, 
+      assigned_to: req.body.assigned_to 
+    })
+    .then((cards) =>{
+      res.send('your stuff posted');
+    });
+})
+
+router.route('/:id')
+  .put((req,res) =>{
+    Cards.update({
+      title : req.body.title,
+      priority: req.body.priority,
+      created_by: req.body.created_by, 
+      assigned_to: req.body.assigned_to 
+    },{
+      where : {id:req.params.id}
+    })
+    .then((cards) =>{
+      res.send('your card has been updated');
+    })
+  })
+  .delete((req, res) =>{
+    Cards.destroy({
+      where: {id: req.params.id}
+    })
+    .then((cards) =>{
+      res.send('you finished your task');
+    })
+  })
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 /*jshint esversion:6 */
 
 const express = require('express');
-const app = exress();
+const app = express();
 const CONFIG = require('./config/config');
-  
+const bodyParser = require ('body-parser');
+const methodOverride = require('method-override');
+
+const db = require('./models');
+let Cards = db.Card;
+const cardsRoute = require('./routes/cards');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/api/kanban/todo', cardsRoute);
+
+app.get('/', (req, res) =>{
+  res.send('fawka is working');
+});
+
+app.listen(8080, function(){
+  console.log('sever started');
+  db.sequelize.sync();
+});
+
+module.exports = app;
