1ccdc71f94a62d1690c68eb85bf5f075f0ee5a6a
raaarwwwwandy <hirandysan@gmail.com>
2017-03-23 15:19:03 -1000

    client and server files are seperated now and working properly

---
+++ client/src/containers/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Card from '../../components/Cards.js';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super();
+
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Kanban</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
