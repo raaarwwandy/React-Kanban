eb680029f76bf00e5d877a573ad88853bf3fb91c
raaarwwwwandy <hirandysan@gmail.com>
2017-03-25 13:49:57 -1000

    card component to render it on page

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Card = (props) =>(
-  <li>
+  <ul>
     <h1>{props.title}</h1>
     <h3>{props.priority}</h3>
     <h3>{props.created_by}</h3>
     <h3>{props.assigned_to}</h3>
-  </li>
+  </ul>
 );
 
 export default Card;
