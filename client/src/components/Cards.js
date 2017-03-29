ae2bc6f73d5d51379938cb469a67a41444c46ffb
raaarwwwwandy <hirandysan@gmail.com>
2017-03-29 00:09:24 -1000

    re-wrote Cards as a extended component

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = (props) =>(
-  <ul>
-    <h1>{props.title}</h1>
-    <h3>{props.priority}</h3>
-    <h3>{props.created_by}</h3>
-    <h3>{props.assigned_to}</h3>
-  </ul>
-);
+class Card extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className='singleCard'>
+      <h2>Task: {this.props.title}</h2>
+      <h3>Status: {this.props.status}</h3>
+      <h3>Priority: {this.props.priority}</h3>
+      <h3>Assigned To: {this.props.assigned_to}</h3>
+      <h3>Assigned By: {this.props.assigned_by}</h3>
+      </div>
+    )
+  }
+  
+};
 
 export default Card;
+
+
