bb511600c922b2741f8e8c1148989f2830c068de
raaarwwwwandy <hirandysan@gmail.com>
2017-03-29 00:11:42 -1000

    progess display is only showing card with progess as the status

--- client/src/components/ProgressDisplay.js
+++ client/src/components/ProgressDisplay.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Card from '../components/Cards.js';
+// const QueueDisplay = (props) => (
+//   <div className="QueueCards">
+//     <h1>Task: {props.title}</h1>
+//     <h3>Priority: {props.priority}</h3>
+//     <h3>Status: {props.status}</h3>
+//     <h3>Assigened By: {props.created_by}</h3>
+//     <h3>Assigned to: {props.assigned_to}</h3>
+//   </div>
 
-class ProgressDisplay extends React.Component{
-  constructor(props) {
-    super(props);
-    
-  }
+// );
 
+  class ProgressDisplay extends Component {
+    constructor(props){
+      super(props);
+    }
+  
 
-
-render(){
-  return(
+  render(){
+    // console.log(this.props)
+    return(
+      <div className="queue">
+        <h1>In Progress</h1>
+        {this.props.cards.filter((card) => card.status ==='PROGRESS').map((cards) =>{
+          return(<Card 
+            _key={cards.key}
+            title={cards.title}
+            status={cards.status}
+            priority={cards.priority}
+            assigned_to={cards.assigned_to}
+            />
+            )
+        })
+      }
+      </div>
     )
   }
-
 }
+
+export default ProgressDisplay;
