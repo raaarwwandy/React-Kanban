9984bf930423c6205c55c1bc2951f0dee46f7825
raaarwwwwandy <hirandysan@gmail.com>
2017-03-29 00:14:07 -1000

    some change

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './containers/app/App';
 import './index.css';
 
+import{ Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import cards from './reducers';
+import ReduxThunk from 'redux-thunk';
+import { createDevTools } from 'redux-devtools'
+
+
+//reducers --------------
+let store = createStore(
+  cards, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(ReduxThunk)
+  );
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
