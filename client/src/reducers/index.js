176fddcd2d143bfc8e86baa23ee210fc97f5ec64
raaarwwwwandy <hirandysan@gmail.com>
2017-03-29 00:14:37 -1000

    reducer is properly storing

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,24 +5,22 @@ const initialState = {
 }
 
 function cards(state = initialState, action){
-  console.log('state of store before reducing', state);
   switch(action.type){
     case ADD_CARD:
-    let temp = Object.assign({}, state, {
+    return Object.assign({}, state, {
         cards: [
           ...state.cards, {
             title: action.title, 
+            status: action.status,
             priority: action.priority, 
             created_by: action.created_by,
             assigned_to: action.assigened_to
           }
         ]
       })
-      console.log('state of store after reducing', temp)
-      return temp
       default: 
         return state;
-  }
+  } 
 }
 
 export default cards;
